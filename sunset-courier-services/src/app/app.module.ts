7c7a40256f9c9b469a971c3d1f132d2c013121c0
Bhavini <bgor13@gmail.com>
2020-05-02 16:26:56 +0100

    Added Anular Materials Created new components

--- sunset-courier-services/src/app/app.module.ts
+++ sunset-courier-services/src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { ContactComponent } from './contact/contact.component';
+import { QuotationComponent } from './quotation/quotation.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactUsComponent,
+    ContactComponent,
+    QuotationComponent,
+    ServicesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
