a9b6b0ce1c1f38ce7e040be82c90d533ffee36ac
Rahul Vaghela <rahul.vaghela@21stplc.com>
2020-06-08 15:46:11 +0100

    added images to carousel

--- sunset-courier-services/src/app/image-carousel/image-carousel.component.ts
+++ sunset-courier-services/src/app/image-carousel/image-carousel.component.ts
@@ -1,23 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-image-carousel',
   templateUrl: './image-carousel.component.html',
-  styleUrls: ['./image-carousel.component.css']
+  styleUrls: ['./image-carousel.component.css'],
 })
 export class ImageCarouselComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
     console.log(this.slides);
   }
 
-
-  public images = ['../parcel-delivery.jpg','../parcel-delivery.jpg','../parcel-delivery.jpg','../parcel-delivery.jpg','../parcel-delivery.jpg'];
-
+  public images = [
+    './app/images/51090470-young-delivery-men-loading-cardboard-boxes-in-truck.jpg',
+    '../98924574-packages-with-supplies.jpg',
+    '../stock-photo-courier-is-loading-the-van-with-parcels-661996507.jpg',
+    '../parcel-delivery.jpg',
+  ];
 
   public slides: any = this.images;
-
 }
